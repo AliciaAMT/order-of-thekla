9ab7bf522b183ddb8346c06bd28f3a8b8a29d17c
Alicia A Taylor <aliciataylorguitar@gmail.com>
2025-08-24 19:04:31 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'order-of-thekla',
+  webDir: 'www'
+};
+
+export default config;
